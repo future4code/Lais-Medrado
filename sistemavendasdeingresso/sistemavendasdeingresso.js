c4998a007acf57ca8246f04f45ee8fc131825b40
Nome <laismedrado@live.com>
2021-09-27 09:38:39 -0300

    exercicios de arrays e strings adicionando  a parte do desafio

---
+++ sistemavendasdeingresso/sistemavendasdeingresso.js
@@ -0,0 +1,99 @@
+          //Projeto desenvolvido na linguagem javascripts de desenvolvimento  de um sistema de vendas 
+          //de ingressos para  jogos de um estádio de futebol. Para que funcione o  
+          //o usuário deverá fornecer algumas informações:
+
+
+          // 1- nome completo
+
+          // 2- tipo de jogo: IN indica internacional; 
+          // e DO indica doméstico;
+
+          // 3-Categoria: pode ser as opções 1, 2, 3 ou 4
+
+          // 4-quantidade de ingressos
+
+
+          //O sistema vai solicitar estas informações ao usuário, através do prompt . Além disso, 
+          //ele vai imprimir tudo isso, junto com o valor de cada ingresso e o valor total que o usuário 
+          //tem que pagar (ou seja, o valor unitário do ingresso multiplicado pela quantidade), valores estes
+          //dados em tabela  pela empresa responsável,para desenvolvimento do programa. 
+          //Lembrando que o valor de jogos internacionais é o mesmo de jogos domésticos, mas seus preços devem 
+          //ser multiplicados pelo valor do dólar (considerar o dólar = R$4,10)
+
+          // ---Dados da compra--- 
+          //   Nome do cliente:  
+          //   Tipo do jogo:  
+          //   Etapa do jogo:  
+          //   Categoria:   
+          //   Quantidade de Ingressos:  
+          //   ---Valores--- 
+          //   Valor do ingresso:  R$ 
+          //   Valor total:  R$ 
+          
+
+          //____________________________________________________________________________
+
+          let nome = prompt("Digite aqui seu nome :");
+          let tipoDoJogo = prompt(" Digite aqui o tipo do jogo :  IN - para jogo  internacional e DO -para jogo doméstico: ");
+          let etapaDoJogo = prompt("Digite aqui a etapa do jogo: SF indica semi-final; DT indica decisão de terceiro lugar; e FI indica final:");
+          let categoria = prompt("Digite aqui qual categoria dessas : 1-2-3-4 : ");
+          let quantidadeDeIngressos = Number(prompt("Digite aqui a quantidade de ingressos:"));
+          let preco;
+          let tipoDoJogoExtenso = 'Doméstico'
+        
+          if (etapaDoJogo === "SF") {
+            if (categoria === "1") {
+              preco = 1320;
+            } else if (categoria === "2") {
+              preco = 880.00;
+            } else if (categoria === "3") {
+              preco = 550.00;
+            } else if (categoria === "4") {
+              preco = 220.00;
+            }
+          } else if (etapaDoJogo === "DT") {
+            if (categoria === "1") {
+              preco = 660.00;
+            } else if (categoria === "2") {
+              preco = 440.00;
+            } else if (categoria === "3") {
+              preco = 330.00;
+            } else if (categoria === "4") {
+              preco = 170.00;
+            }
+          } else if (etapaDoJogo === "FI") {
+            if (categoria === "1") {
+              preco = 1980.00;
+            } else if (categoria === "2") {
+              preco = 1320.00;
+            } else if (categoria === "3") {
+              preco = 880.00;
+            } else if (categoria === "4") {
+              preco = 330.00;
+            }
+          }
+        
+          if (tipoDoJogo === "IN") {
+            preco = preco * 4.20;
+            tipoDoJogoExtenso = 'Internacional'
+          }
+        
+          let valorTotal = (preco * quantidadeDeIngressos);
+        
+        
+        
+          console.log("DADOS DA COMPRA");
+          console.log("Nome do cliente", nome);
+          console.log("Tipo do jogo", tipoDoJogoExtenso);
+          console.log("Etapa do jogo", etapaDoJogo);
+          console.log("Categoria", categoria);
+          console.log("Quantidade de ingressos", quantidadeDeIngressos);
+          console.log(" -------VALORES------");
+          console.log("Valor do ingresso:", preco);
+          console.log("Valor total:", valorTotal);
+        
+        
+        }
+        
+        
+        calcularValorIngresso();
