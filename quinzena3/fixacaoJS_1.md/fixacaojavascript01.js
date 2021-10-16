afcf0d96bab82686e3071ed59ebe96eb04acb7dd
Nome <laismedrado@live.com>
2021-10-16 01:12:53 -0300

    exercicio fixacao javascript 01

---
+++ quinzena3/fixacaoJS_1.md/fixacaojavascript01.js
@@ -0,0 +1,18 @@
+//exercicio de fixação javascript 01
+
+
+
+
+function calculaSalario(qtdeCarrosVendidos, valorTotalVendas) {
+    // Escreva seu código aqui
+   const salario = 2000
+   const comissao = 100 
+   const porcentagem = 0.05
+   let valorDaComissao = (qtdeCarrosVendidos * comissao) + (valorTotalVendas * porcentagem)
+   
+   const salarioMensal = salario + valorDaComissao
+   
+   return salarioMensal
+   }
+
+
